904ead38ff38e3e5858fe798e92ec2f3bb013115
Eddie Mattia <eddie@outerbounds.com>
2023-08-17 09:45:40 -0700

    add worker heartbeat

--- metaflow/plugins/frameworks/ray.py
+++ metaflow/plugins/frameworks/ray.py
@@ -1,11 +1,7 @@
-import inspect
 import subprocess
-import pickle
-import tempfile
 import os
 import sys
 import time
-import logging
 from metaflow.unbounded_foreach import UBF_CONTROL
 from metaflow.plugins.parallel_decorator import ParallelDecorator, _local_multinode_control_task_step_func
 
@@ -20,13 +16,17 @@ class RayParallelDecorator(ParallelDecorator):
         self, step_func, flow, graph, retry_count, max_user_code_retries, ubf_context
     ):
 
-        # from metaflow import current
-        import socket
         from functools import partial
+
+        def _worker_heartbeat(graph_info=flow._graph_info):
+            from metaflow import Task, current
+            control = get_previous_task_pathspec(graph_info, current)
+            while not Task(control).finished:
+                time.sleep(3)
 
         def _empty_worker_task():
             pass
-        
+
         if os.environ.get("METAFLOW_RUNTIME_ENVIRONMENT", "local") == "local":
             if ubf_context == UBF_CONTROL:
                 env_to_use = getattr(self.environment, "base_env", self.environment)
@@ -42,11 +42,19 @@ class RayParallelDecorator(ParallelDecorator):
             self.setup_distributed_env(flow, ubf_context)
             if ubf_context == UBF_CONTROL:
                 return step_func 
-            return partial(_empty_worker_task) # don't need to run code on worker task. ray.init() in control attaches driver to the cluster.       
+            return partial(_worker_heartbeat) # don't need to run user code on worker task. ray.init() in control attaches driver to the cluster.       
 
     def setup_distributed_env(self, flow, ubf_context):
-        ray_cli_path = sys.executable.replace("python", "ray") # assumes ray is installed in the environment via user code or base image. 
+        self.ensure_ray_air_installed()
+        ray_cli_path = sys.executable.replace("python", "ray") 
         setup_ray_distributed(self.attributes["main_port"], ray_cli_path, flow, ubf_context)
+
+    def ensure_ray_air_installed(self):
+        try:
+            import ray
+        except ImportError:
+            print("Ray is not installed. Installing latest version of ray-air package.")
+            subprocess.run([sys.executable, "-m", "pip", "install", "-U", "ray-air"])
 
 
 def setup_ray_distributed(
@@ -63,12 +71,12 @@ def setup_ray_distributed(
 
     # Why are deco.task_pre_step and deco.task_decorate calls in the same loop?
     # https://github.com/Netflix/metaflow/blob/76eee802cba1983dffe7e7731dd8e31e2992e59b/metaflow/task.py#L553
-        # this causes the following current.parallel variables to be defaults on all nodes here,
+        # this causes these current.parallel variables to be defaults on all nodes,
         # since AWS Batch decorator's task_pre_step hasn't run yet.
     # num_nodes = current.parallel.num_nodes
     # node_index = current.parallel.node_index
 
-    # Workaround that doesn't generalize to non-AWS compute providers.
+    # AWS Batch-specific workaround.
     num_nodes = int(os.environ["AWS_BATCH_JOB_NUM_NODES"])
     node_index = os.environ["AWS_BATCH_JOB_NODE_INDEX"]
     node_key = os.path.join("ray_nodes", "node_%s.json" % node_index)
@@ -138,3 +146,29 @@ def setup_ray_distributed(
             time.sleep(10)
 
     s3.close()
+
+
+def get_previous_task_pathspec(graph_info, current):
+    """
+    Find the pathspec of the control task that a worker task is coupled to. 
+    """
+ 
+    from metaflow import Step
+
+    steps_info = graph_info['steps']
+    for step_name, step_info in steps_info.items():
+        if current.step_name == step_name:
+            previous_step_name = step_name
+            step_pathspec = "{flow_name}/{run_id}/{step_name}".format(
+                flow_name=current.flow_name,
+                run_id=current.run_id,
+                step_name=previous_step_name
+            )
+            step = Step(step_pathspec)
+            for task in step:
+                if task.id.startswith("control"):
+                    control_task_pathspec = "{step_pathspec}/{task_id}".format(
+                        step_pathspec=step.pathspec,
+                        task_id=task.id
+                    )
+                    return control_task_pathspec
